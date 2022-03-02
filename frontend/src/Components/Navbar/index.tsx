f5dbae0330ca9a1b9e9665dd3c97a63ac521104b
rafaelainocencio <rafaelainocencioo@gmail.com>
2022-03-02 16:55:51 -0300

    ajustando2

--- frontend/src/Components/Navbar/index.tsx
+++ frontend/src/Components/Navbar/index.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light ">
             <Link to="/">
-                <a className="navbar-brand">RecodeAir</a>
+                <button className="navbar-brand">RecodeAir</button>
             </Link>
             <div className='opcoes'>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
@@ -13,9 +13,9 @@ function Navbar() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        
+
                         <Link to="/destinos">
-                            <a className="nav-item nav-link">Destinos</a>
+                            <button className="nav-item nav-link">Destinos</button>
                         </Link>
 
                         {/* <Link to="/sobre">
@@ -23,7 +23,7 @@ function Navbar() {
                         </Link> */}
                         
                         <Link to="/form">
-                            <a className="nav-item nav-link">CRUD</a>
+                            <button className="nav-item nav-link">CRUD</button>
                         </Link>
 
                     </div>
