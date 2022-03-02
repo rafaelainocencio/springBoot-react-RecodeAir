5f128251fd1d7efd0043a8b1a20618cd9c7f7ee2
rafaelainocencio <rafaelainocencioo@gmail.com>
2022-03-02 13:46:26 -0300

    novo frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css'
-
-import{
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom'
-
-import Navbar from './components/Navbar';
-import Destinos from './Pages/Destinos';
-import Form from './Pages/Form';
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/destinos' element={<Destinos/>} />
-        <Route path='/form' element={<Form/>}>
-          <Route path=':destinoId' element={<Form/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
